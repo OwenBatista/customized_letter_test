78d04f05087c51be56c5b29ea53b5ad85c09a683
Owen Batista Roman <48600146+OwenBatista@users.noreply.github.com>
2024-12-18 15:39:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ PLACEHOLDER = "[name]"
 
 #Opens the file that contains a list of invited names and read all the lines.
 #Each line in the file corresponds to a name
-with open("file_merge_stuff/Input/Letters/Names/invited_names.txt") as names_file: 
+with open("./Input/Names/invited_names.txt") as names_file: 
     names = names_file.readlines() #Reads all the names into a list, where each item is a name string
 
 #Opens file that contains ste starting letter template and read its content
-with open("file_merge_stuff/Input/Letters/starting_letter.txt") as letter_file:
+with open("./Input/Letters/starting_letter.txt") as letter_file:
     letter_content = letter_file.read() #Read the entire template letter into a single string
     #Iterate over each name in the list of names
     for name in names:
         stripped_name = name.strip() #Removes any trailing whitespace from name
         new_letter = letter_content.replace(PLACEHOLDER, stripped_name) #Replace placeholder in the letter template with current name
         #Create new file for completed letter addressed to the current name
-        with open(f"file_merge_stuff/Output/ReadyToSend/Letter_for_{stripped_name}", mode="w") as completed_letter:
+        with open(f"./Output/ReadyToSend/Letter_for_{stripped_name}", mode="w") as completed_letter:
             completed_letter.write(new_letter) #Writes personalized letter content to the new file
